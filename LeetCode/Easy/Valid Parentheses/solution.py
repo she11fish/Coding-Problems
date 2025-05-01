19b1f880aaa85818caeca0fc20e7e3694b42ee35
Wahib <99519508+she11fish@users.noreply.github.com>
2025-05-01 15:38:15 -0400

    better Valid Parentheses solution

--- LeetCode/Easy/Valid Parentheses/solution.py
+++ LeetCode/Easy/Valid Parentheses/solution.py
@@ -1,21 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        opening_brackets = {"{": "}" , "(": ")", "[": "]"}
         stack = []
+        brackets = {
+            "(": ")",
+            "[": "]",
+            "{": "}"
+
+        }
         for char in s:
-            if char in opening_brackets:
+            if char in brackets:
                 stack.append(char)
-            else:
-                if not len(stack):
-                    return False
-                current_char = stack.pop()
-                while True:
-                    if current_char in opening_brackets:
-                        if opening_brackets[current_char] == char:
-                            break
-                        else:
-                            return False
-                    if not len(stack):
-                        return False
-                    current_char = stack.pop()
-        return not len(stack)
+                continue
+            if not stack or brackets[stack[-1]] != char:
+                return False
+            stack.pop()
+        return not stack
